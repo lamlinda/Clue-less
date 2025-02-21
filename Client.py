a30dbe3b6134938a0bbd3893ffc69fce8103ca1c
Lamli <nguyenli23@up.edu>
2025-02-20 21:31:56 -0700

    Add functionality to print board for client and allow them to move the player around the board

---
+++ Client.py
@@ -0,0 +1,28 @@
+import socket
+
+def main():
+    client_socket = startClient()
+
+    while True:
+        data = client_socket.recv(1024)
+        print(f"Server: \n {data.decode()}")
+        
+        msg = input("You: ")
+        if msg.lower() == 'exit':
+            break
+        client_socket.sendall(msg.encode())
+
+    client_socket.close()
+
+
+def startClient():
+    HOST = '127.0.0.1'  # Server address
+    PORT = 12345        # Server port
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+
+    return client_socket
+
+if __name__=="__main__":
+    main()
