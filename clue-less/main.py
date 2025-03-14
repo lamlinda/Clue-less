ecf417010b35d5f5c43e484cde3144f02f87c662
neon-deon <202967809+neon-deon@users.noreply.github.com>
2025-03-13 20:54:50 -0500

    Add files via upload
    Files with folder structure

---
+++ clue-less/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from extensions import db, socketio
+from routes.lobby import lobby_bp
+
+HOST = "0.0.0.0"
+PORT = 5000
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+
+db.init_app(app)
+socketio.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+
+app.register_blueprint(lobby_bp, url_prefix='/lobby')
+
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, host=HOST, port=PORT, debug=True)
