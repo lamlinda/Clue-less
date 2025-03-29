74b9595bf0eaaea8d891fda654ac69c8c7c811ec
Lyhong Peou <lpeou2@jh.edu>
2025-03-29 16:28:46 -0400

    Refec - break down models.py

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .lobby import Lobby
+from .player import Player
+
+__all__ = ['Lobby', 'Player']
+# This file is used to import all models in the models package.
---
+++ models/player.py
@@ -0,0 +1,30 @@
+import uuid
+from extensions import db
+import random
+import string
+import json
+
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+
+def generate_lobby_id():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+class Player(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
+    lobby_id = db.Column(db.String(36), db.ForeignKey('lobby.id'), nullable=True)
+    name = db.Column(db.String(50), nullable=False)
+    character = db.Column(db.String(50), nullable=True)
+    eliminated = db.Column(db.Boolean, default=False)  # If player made incorrect accusation
+
+    def __init__(self, name, lobby_id=None):
+        self.id = generate_uuid()
+        self.name = name
+        self.lobby_id = lobby_id
+        self.character = None
+        self.eliminated = False
+
+    def __repr__(self):
+        return f"Player('{self.id}', '{self.name}')"
